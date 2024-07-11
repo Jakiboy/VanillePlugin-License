c748c384771420387f663f2bfaf4def2b8b77592
Jakiboy <j.sinnaour.official@gmail.com>
2024-07-11 19:53:37 +0100

    Update

---
+++ src/License.php
@@ -0,0 +1,391 @@
+<?php
+/**
+ * @author    : Jakiboy
+ * @package   : VanillePlugin
+ * @version   : 1.0.x
+ * @copyright : (c) 2018 - 2024 Jihad Sinnaour <mail@jihadsinnaour.com>
+ * @link      : https://jakiboy.github.io/VanillePlugin/
+ * @license   : MIT
+ *
+ * This file if a part of VanillePlugin Framework.
+ */
+
+declare(strict_types=1);
+
+namespace VanilleLicense;
+
+class License extends Webservice
+{
+	use \VanillePlugin\tr\TraitDatable;
+
+	/**
+	 * @access protected
+	 */
+	protected const OPTION  = 'activation';
+	protected const DATA    = 'license';
+	protected const VERSION = 'lite';
+	protected const PACK    = '1s';
+	protected const DATE    = 'd/m/Y H:i';
+	protected const EXPIRE  = 0;
+	protected const MULTI   = false;
+	protected const CONTENT = [
+		'version' => null,
+		'pack'    => null,
+		'date'    => null,
+		'expire'  => null,
+		'quota'   => []
+	];
+	protected const CREDENTIALS = [
+		'token' => null,
+		'user'  => null,
+		'pswd'  => null,
+		'key'   => null
+	];
+
+	/**
+	 * @access protected
+	 */
+	protected const MESSAGE = [
+		'unknown-error'    => 'Unknown error, please contact support',
+		'server-down'      => 'Unable to connect to server, please try again',
+		'empty-response'   => 'No response, please try again',
+		'expired-license'  => 'Your license is expired, please renew it',
+		'invalid-response' => 'Invalid response, please try again',
+		'invalid-version'  => 'Invalid license version, please upgrade your license',
+		'invalid-key'      => 'Your license key is invalid, please buy new license',
+		'invalid-format'   => 'Your license key is invalid, please check your entry',
+		'invalid-auth'     => 'Please verify your email and password',
+		'invalid-quota'    => 'Your quota domain limit reached! please upgrader your license',
+		'missing-domain'   => 'Your website is not authorized, please contact support',
+		'missing-key'      => 'Your license key is invalid, please contact support',
+	];
+
+	/**
+	 * @access protected
+	 */
+	protected const ERROR = [
+		'invalid-auth'     => 'Unauthorized',
+		'invalid-key'      => 'Invalid license key',
+		'invalid-format'   => 'Invalid license key format',
+		'invalid-version'  => 'Invalid license version',
+		'invalid-quota'    => 'Quota domain limit reached',
+		'expired-license'  => 'Expired license',
+		'missing-domain'   => 'Domain not provided',
+		'missing-key'      => 'License key not provided',
+	];
+
+	/**
+	 * @inheritdoc
+	 */
+	public function __construct(array $args = [])
+	{
+		parent::__construct($this->getCredentials(), $args);
+	}
+
+	/**
+	 * Load license file.
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function load()
+	{
+		$license = $this->getRoot('/.license');
+		if ( $this->isReadable($license, true) ) {
+			$token = $this->readFile($license);
+			$data  = $this->getCredentials();
+			$data['token'] = (string)$token;
+			if ( $this->setCredentials($data) ) {
+				$this->auth = $data;
+				$this->isValid();
+				$this->removeFile($license);
+			}
+		}
+	}
+
+	/**
+	 * Activate license.
+	 *
+	 * @access public
+	 * @param bool $status
+	 * @return array
+	 */
+	public function activate(?bool &$status = null) : array
+	{
+		$this->auth = $this->parseCredentials();
+		$status = $this->isValid($data);
+		return $data;
+	}
+
+	/**
+	 * Check whether license is valid.
+	 *
+	 * @access public
+	 * @return bool
+	 */
+	public function isValid(?array &$data = []) : bool
+	{
+		// Server down
+		if ( $this->check()->isDown() ) {
+			$this->setBaseUrl((string)static::BACKUP);
+		}
+
+		// Backup server down
+		if ( $this->check()->isDown() ) {
+			return true;
+		}
+		
+		// Get response
+		$data = $this->decodeJson($this->body(), true);
+
+		// No response
+		if ( !$data ) {
+			$this->disable(static::MESSAGE['empty-response']);
+			return false;
+		}
+
+		// Response error
+		if ( $this->hasError() ) {
+			$this->disable($this->parseError($data));
+			return false;
+		}
+
+		$code   = $data['code'] ?? false;
+		$status = $data['status'] ?? false;
+
+		if ( $code == self::UP && $status == 'success' ) {
+
+			// Version error
+			$content = $data['content'] ?? [];
+			$content = $this->mergeArray(static::CONTENT, $content);
+			
+			if ( $content['version'] == 'api' ) {
+				$this->disable(static::MESSAGE['invalid-version']);
+				return false;
+			}
+
+			$this->enable($content);
+			return true;
+
+		}
+
+		return false;
+	}
+
+	/**
+	 * Get license data.
+	 *
+	 * @access public
+	 * @return array
+	 */
+	public function getData() : array
+	{
+		return (array)$this->getPluginOption(
+			static::DATA,
+			static::CONTENT,
+			static::MULTI
+		);
+	}
+
+	/**
+	 * Set license data.
+	 *
+	 * @access public
+	 * @param array $data
+	 * @return bool
+	 */
+	public function setData(array $data) : bool
+	{
+		$data = $this->mergeArray($this->getData(), $data);
+		return $this->updatePluginOption(static::DATA, $data, static::MULTI);
+	}
+
+	/**
+	 * Get license version.
+	 *
+	 * @access public
+	 * @return string
+	 */
+	public function getVersion() : string
+	{
+		$data = $this->getData();
+		$version = $data['version'] ?? static::VERSION;
+		return (string)$version;
+	}
+
+	/**
+	 * Get license expiration.
+	 *
+	 * @access public
+	 * @return int
+	 */
+	public function getExpire() : int
+	{
+		$data = $this->getData();
+		$expire = $data['expire'] ?? static::EXPIRE;
+		return (int)$expire;
+	}
+
+	/**
+	 * Check license expiration.
+	 *
+	 * @access public
+	 * @return bool
+	 */
+	public function isExpired() : bool
+	{
+		return ($this->getExpire() === static::EXPIRE);
+	}
+
+	/**
+	 * Check new install.
+	 *
+	 * @access public
+	 * @return bool
+	 */
+	public function isNew() : bool
+	{
+		$data = $this->getData();
+		$expire = $data['expire'] ?? null;
+		return ($expire === null);
+	}
+
+	/**
+	 * Check whether license is activated (Alias).
+	 *
+	 * @access public
+	 * @return bool
+	 */
+	public function isActivated() : bool
+	{
+		return !$this->isExpired();
+	}
+
+	/**
+	 * Check whether license is disabled (Alias).
+	 *
+	 * @access public
+	 * @return bool
+	 */
+	public function isDisabled() : bool
+	{
+		return $this->isExpired();
+	}
+
+	/**
+	 * Reset license data.
+	 *
+	 * @access public
+	 * @return bool
+	 */
+	public function reset() : bool
+	{
+		$this->deletePluginTransient('license-error');
+		$this->setCredentials(static::CREDENTIALS);
+		return $this->setData(static::CONTENT);
+	}
+	
+	/**
+	 * Index endpoint.
+     * [GET: /].
+	 *
+	 * @access protected
+	 * @return object
+	 */
+	protected function index() : self
+	{
+		return $this->send('/');
+	}
+
+	/**
+	 * Check license endpoint.
+     * [GET: /license/check/].
+	 *
+	 * @access protected
+	 * @return object
+	 */
+	protected function check() : self
+	{
+		$this->setBody([
+			'domain' => $this->geSiteDomain(),
+			'key'    => $this->auth['key'] ?? false
+		]);
+		return $this->send('/license/check/');
+	}
+
+	/**
+	 * Get license credentials from option.
+	 *
+	 * @access protected
+	 * @return array
+	 */
+	protected function getCredentials() : array
+	{
+		$data = $this->getPluginOption(static::OPTION, [], static::MULTI);
+		return $data ?: [];
+	}
+
+	/**
+	 * Parse license credentials from request.
+	 *
+	 * @access protected
+	 * @return array
+	 */
+	protected function parseCredentials() : array
+	{
+		$data = $this->getHttpPost(static::OPTION, true);
+		$this->updatePluginOption(static::OPTION, $data, static::MULTI);
+		return (array)$data;
+	}
+
+	/**
+	 * Set license credentials.
+	 *
+	 * @access protected
+	 * @param array $data
+	 * @return bool
+	 */
+	protected function setCredentials(array $data) : bool
+	{
+		return $this->updatePluginOption(static::OPTION, $data, static::MULTI);
+	}
+
+	/**
+	 * Disable license.
+	 *
+	 * @access protected
+	 * @param string $error
+	 * @return bool
+	 */
+	protected function disable(?string $error = null) : bool
+	{
+		if ( $error ) {
+			$this->setPluginTransient('license-error', $error);
+		}
+		return $this->setData(['expire' => static::EXPIRE]);
+	}
+
+	/**
+	 * Enable license.
+	 *
+	 * @access protected
+	 * @param array $content
+	 * @return bool
+	 */
+	protected function enable(array $content) : bool
+	{
+		$current = $this->getDate('now', 'Y-m-d H:i:s', true);
+		$expire  = $content['expire'] ?? $current;
+		$date    = $this->createDate($expire, static::DATE);
+		$expire  = $this->getDateDiff($current, $date, '%R%a') + 1;
+		$date    = $this->dateToString($date, static::DATE);
+
+		$this->deletePluginTransient('license-error');
+		return $this->setData([
+			'expire'  => $expire,
+			'date'    => $date,
+			'version' => $content['version'] ?? static::VERSION,
+			'pack'    => $content['pack'] ?? static::PACK
+		]);
+	}
+}
--- src/Webservice.php
+++ src/Webservice.php
@@ -14,6 +14,80 @@ declare(strict_types=1);
 
 namespace VanilleLicense;
 
-class Webservice
+use VanillePlugin\lib\API;
+
+class Webservice extends API
 {
+	/**
+	 * @access public
+	 * @var int DOWN, Down status code
+	 * @var string HOST, Server url
+	 * @var string BACKUP, Backup server url
+	 */
+	public const DOWN   = 429;
+	public const HOST   = null;
+	public const BACKUP = null;
+
+	/**
+	 * @access protected
+	 * @var array MESSAGE, Server message
+	 * @var array ERROR, Server error
+	 * @var array CONTENT, Server default content
+	 */
+	protected const MESSAGE = [
+		'unknown-error' => 'Unknown error, please contact support'
+	];
+	protected const ERROR   = [];
+	protected const CONTENT = [];
+	
+	/**
+	 * Init Webservice request.
+	 * [Filter: {plugin}-webservice-timeout].
+	 *
+	 * @access protected
+	 * @param array $auth
+	 * @param array $args
+	 */
+	protected function __construct(array $auth = [], array $args = [])
+	{
+		// Set auth
+		$this->auth = $this->mergeArray($this->getRemoteServer(), $auth);
+
+		// Set host
+		$url = static::HOST ?: $this->auth['host'];
+
+		$timeout = $this->applyPluginFilter('webservice-timeout', 30);
+		$args = $this->mergeArray([
+			'timeout' => $timeout
+		], $args);
+
+		parent::__construct(self::GET, $args, $url);
+	}
+
+	/**
+	 * Index.
+     * [GET: /].
+	 *
+	 * @access protected
+	 * @return object
+	 */
+	protected function index() : self
+	{
+		return $this->send('/');
+	}
+
+	/**
+	 * Parse response error.
+	 *
+	 * @access protected
+	 * @param array $response
+	 * @return string
+	 */
+	protected function parseError(array $response) : string
+	{
+		$message = $response['message'] ?? false;
+		$error = $this->arrayKeys(static::ERROR, $message, true);
+		$error = $error[0] ?? 'unknown-error';
+		return static::MESSAGE[$error];
+	}
 }
